3c2d077aa4bfc1368483c0103ec4fd21d9cee082
Fairy <dengyuhy@qq.com>
2022-04-23 18:38:31 +0800

    Login Finished

--- Service/src/main/java/com/unicorn/service/controller/LoginController.java
+++ Service/src/main/java/com/unicorn/service/controller/LoginController.java
@@ -1,9 +1,7 @@
 package com.unicorn.service.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Objects;
@@ -20,12 +18,14 @@ public class LoginController {
     @ResponseBody
     @RequestMapping(value = "/UserLoginCheck", method = RequestMethod.POST)
     public String userLoginCheckController(String userName, String userPassWord) {
+        System.out.println(userName);
+        System.out.println(userPassWord);
         if (!Objects.equals(userName, "unicorn")) {
             return "UserName is Wrong";
         } else if (!Objects.equals(userPassWord, "123456")) {
             return "PassWord is Wrong";
         } else {
-            return "OK,Pass!";
+            return "true";
         }
     }
 }
