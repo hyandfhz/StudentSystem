eaba0f4b630b2234a19b0d73ddca898ad387908b
Fairy <dengyuhy@qq.com>
2022-04-23 09:30:09 +0800

    created user

--- Service/src/main/java/com/unicorn/service/dao/Users.java
+++ Service/src/main/java/com/unicorn/service/dao/Users.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Repository
 public class Users {
@@ -13,10 +16,16 @@ public class Users {
     ReadDirectory directoryReader;
 
     public boolean userExists(String type, String userName) {
-        return getUser(type, userName).exists();
+        return getUserDirectory(type, userName).exists();
     }
 
-    public File getUser(String type, String userName) {
+    public File getUserDirectory(String type, String userName) {
         return new File("./data/user/" + type + "/" + userName);
+    }
+
+    public String getUserInformation(File userDirectory, String field) throws IOException {
+
+
+        return fileReader.getFileString(new File(userDirectory, field));
     }
 }
