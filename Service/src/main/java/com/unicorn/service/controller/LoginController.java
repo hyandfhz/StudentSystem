d48a2c9f835dfad9b3ad5abd3017272aef97a2eb
Fairy <dengyuhy@qq.com>
2022-04-24 18:45:56 +0800

    MyBatis - Select Teacher

--- Service/src/main/java/com/unicorn/service/controller/LoginController.java
+++ Service/src/main/java/com/unicorn/service/controller/LoginController.java
@@ -1,13 +1,16 @@
 package com.unicorn.service.controller;
 
+import com.unicorn.service.mapper.TeacherMapper;
+import com.unicorn.service.pojo.Message;
+import com.unicorn.service.pojo.Teacher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Objects;
-
 @Controller
 public class LoginController {
+    TeacherMapper teacherMapper;
+
     @RequestMapping("/Login")
     public ModelAndView loginController() {
         ModelAndView mav = new ModelAndView();
@@ -18,12 +21,24 @@ public class LoginController {
     @ResponseBody
     @RequestMapping(value = "/UserLoginCheck", method = RequestMethod.POST)
     public String userLoginCheckController(String userName, String userPassWord) {
-        if (!Objects.equals(userName, "unicorn")) {
-            return "UserName is Wrong";
-        } else if (!Objects.equals(userPassWord, "123456")) {
-            return "PassWord is Wrong";
-        } else {
-            return "true";
+        Teacher teacher;
+        Message target = new Message();
+        teacher = teacherMapper.getTeacherByLoginId("userName");
+        if (teacher == null) {
+            teacherMapper.getTeacherByLoginId("userName");
+            if (teacher == null) {
+                target.setStatus(false);
+                target.setMessage("用户不存在!请检查您的信息哦!");
+                return target.toString();
+            }
         }
+        if (userPassWord.equals(teacher.getPassWord())) {
+            target.setMessage("欢迎 " + teacher.getTeacherName() + " !");
+            target.setStatus(true);
+        } else {
+            target.setMessage("您是 " + teacher.getTeacherName() + " 吗? 您的密码似乎有误, 请校对后重新登录!");
+            target.setStatus(false);
+        }
+        return target.toString();
     }
 }
---
+++ Service/src/main/java/com/unicorn/service/pojo/Message.java
@@ -0,0 +1,33 @@
+package com.unicorn.service.pojo;
+
+public class Message {
+    String message;
+    boolean status;
+
+    public Message() {
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
